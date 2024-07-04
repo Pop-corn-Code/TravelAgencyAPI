194de5738ec0c3f8caa71db9fc4d41ea851cc730
jery FOTO <85230959+jery123@users.noreply.github.com>
2024-07-04 08:30:53 +0530

    Finalisation of the project by generating the documentation

--- app/Http/Controllers/API/V1/Admin/TravelController.php
+++ app/Http/Controllers/API/V1/Admin/TravelController.php
@@ -7,9 +7,21 @@ use App\Http\Requests\TravelRequest;
 use App\Http\Resources\TravelResource;
 use App\Models\Travel;
 
+/**
+ * @group Admin endpoints
+ */
 class TravelController extends Controller
 {
-    //
+    /**
+     * pOST Travel
+     * 
+     * Create a new Travel record.
+     * 
+     * @authenticated 
+     * 
+     * @response {"data":{"id":"9c55789a-07ea-4a87-b3ac-424a48ebdf6b","name":"Main-travel-update","slug":"travel-2","description":"For testing parameter","number_of_days":"10","number_of_nights":9}} 
+     * @response 422 {"errors":{"name":["The name has already been taken."]}}
+     */
     public function store(TravelRequest $request)
     {
         $travel = Travel::create($request->validated());
